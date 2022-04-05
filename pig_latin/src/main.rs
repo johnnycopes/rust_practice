8c043aab1b7d1a04743c2b1f9c28003374e0e2fa
John Coppola <jdcoppola@gmail.com>
2022-04-05 07:36:51 -0400

    updates program to include alternative solutions

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -8,7 +8,19 @@ fn main() {
     assert_eq!(pig_latinize("Sup"), "up-Say");
     assert_eq!(pig_latinize("apple"), "apple-hay");
     assert_eq!(pig_latinize("Excelsior"), "Excelsior-hay");
+
+    assert_eq!(pig_latinize_slices("hello"), "ello-hay");
+    assert_eq!(pig_latinize_slices("Sup"), "up-Say");
+    assert_eq!(pig_latinize_slices("apple"), "apple-hay");
+    assert_eq!(pig_latinize_slices("Excelsior"), "Excelsior-hay");
+
+    assert_eq!(pig_latinize_mut(String::from("hello")), "ello-hay");
+    assert_eq!(pig_latinize_mut(String::from("Sup")), "up-Say");
+    assert_eq!(pig_latinize_mut(String::from("apple")), "apple-hay");
+    assert_eq!(pig_latinize_mut(String::from("Excelsior")), "Excelsior-hay");
 }
+
+// Initial answer. Not the most efficient because it creates a new String allocation
 
 fn pig_latinize(s: &str) -> String {
     let mut string = s.to_string();
@@ -19,4 +31,37 @@ fn pig_latinize(s: &str) -> String {
         'a' | 'A' | 'e' | 'E' | 'i' | 'I' | 'o' | 'O' | 'u' | 'U' => format!("{}{}-hay", first_char, string),
         _ => format!("{}-{}ay", string, first_char)
     }   
+}
+
+// Alternative answer using only slices
+
+fn pig_latinize_slices(s: &str) -> String {
+    let (first, rest) = (&s[..1], &s[1..]);
+
+    match first {
+        "a" | "A" | "e" | "E" | "i" | "I" | "o" | "O" | "u" | "U" => {
+            format!("{}-hay", s)
+        }
+        _ => format!("{}-{}ay", rest, first)
+    }
+}
+
+// Alternative answer which takes ownership of and mutates the original String
+
+fn pig_latinize_mut(mut s: String) -> String {
+    let first_char = s.remove(0);
+
+    match first_char {
+        'a' | 'A' | 'e' | 'E' | 'i' | 'I' | 'o' | 'O' | 'u' | 'U' => {
+            s.insert(0, first_char);
+            s.push_str("-hay");
+        }
+        _ => {
+            s.push('-');
+            s.push(first_char);
+            s.push_str("ay");
+        }
+    }
+
+    s
 }
